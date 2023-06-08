9851743c918a378f2f1c391fe1a229840d815336
omererben <omer.erben@gmail.com>
2023-06-08 18:25:22 +0300

    Added match and unmatch options

--- src/DriverListPage.js
+++ src/DriverListPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const DriverListPage = () => {
     const drivers = [
@@ -8,12 +8,36 @@ const DriverListPage = () => {
         // Add more drivers as needed
     ];
 
+    const [sentDrivers, setSentDrivers] = useState([]);
+
+    const handleButtonClick = (driverId) => {
+        // Check if the passenger has already been marked as sent
+        const isAlreadySent = sentDrivers.includes(driverId);
+
+        if (isAlreadySent) {
+            // If already sent, remove the passenger from the sentPassengers array
+            setSentDrivers(sentDrivers.filter((id) => id !== driverId));
+        } else {
+            // If not sent, add the passenger to the sentPassengers array
+            setSentDrivers([...sentDrivers, driverId]);
+        }
+    };
+
+    const isDriverSent = (driverId) => sentDrivers.includes(driverId);
+
     return (
-        <div>
+        <div className="passenger-list-container">
             <h2>Driver List</h2>
             <ul>
                 {drivers.map((driver) => (
-                    <li key={driver.id}>{driver.name}</li>
+                    <li key={driver.id}>
+                        <span>{driver.name + " "}</span>
+                        <button
+                            onClick={() => handleButtonClick(driver.id)}
+                            className={isDriverSent(driver.id) ? 'sent' : ''}>
+                            {isDriverSent(driver.id) ? 'Cancel Match' : "Match!"}
+                        </button>
+                    </li>
                 ))}
             </ul>
         </div>
--- src/PassengerListPage.js
+++ src/PassengerListPage.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { MdCheck } from 'react-icons/md';
 
 const PassengerListPage = () => {
     const passengers = [
@@ -8,12 +9,36 @@ const PassengerListPage = () => {
         // Add more passengers as needed
     ];
 
+    const [sentPassengers, setSentPassengers] = useState([]);
+
+    const handleButtonClick = (PassengerId) => {
+        // Check if the passenger has already been marked as sent
+        const isAlreadySent = sentPassengers.includes(PassengerId);
+
+        if (isAlreadySent) {
+            // If already sent, remove the passenger from the sentPassengers array
+            setSentPassengers(sentPassengers.filter((id) => id !== PassengerId));
+        } else {
+            // If not sent, add the passenger to the sentPassengers array
+            setSentPassengers([...sentPassengers, PassengerId]);
+        }
+    };
+
+    const isPassengerSent = (PassengerId) => sentPassengers.includes(PassengerId);
+
     return (
-        <div>
+        <div className="passenger-list-container">
             <h2>Passenger List</h2>
             <ul>
                 {passengers.map((passenger) => (
-                    <li key={passenger.id}>{passenger.name}</li>
+                    <li key={passenger.id}>
+                        <span>{passenger.name + " "}</span>
+                        <button
+                            onClick={() => handleButtonClick(passenger.id)}
+                            className={isPassengerSent(passenger.id) ? 'sent' : ''}>
+                            {isPassengerSent(passenger.id) ? 'Cancel Match' : "Match!"}
+                        </button>
+                    </li>
                 ))}
             </ul>
         </div>
